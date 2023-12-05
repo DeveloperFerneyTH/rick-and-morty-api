fc425c70e3160ba525078b6a477cbef4a5a16e61
Ferney Talero <48497565+DeveloperFerneyTH@users.noreply.github.com>
2023-12-04 21:29:50 -0500

    Delete console log

--- src/helpers/getCharacters.js
+++ src/helpers/getCharacters.js
@@ -3,7 +3,6 @@ import rikosoApi from "../api/rickosoAPI"
 export const getCharater = async (charaterID)=> {
     console.log('ID: '+ charaterID);
     const response = await rikosoApi.get(`/character/${charaterID}`)
-    console.log(response)
     return response.data
 }
 
